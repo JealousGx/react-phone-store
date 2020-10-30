a43e35c306bdb9b6b898fe877e00f402bcc52067
JealousGx <59867217+JealousGx@users.noreply.github.com>
2020-10-30 16:41:25 +0500

    modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
 export default function Footer() {
-    const today = new Date();
     return (
         <div className='container-fluid'>
             <div className='footer bg-dark-gray py-1 mt-5'>
                 <div>
-                <a href='https://github.com/JealousGx/react-phone-store'><i className='fa fa-github icon mt-1' /></a>
-                <a href='https://twitter.com/mateen_khilji'><i class="fab fa-twitter icon mx-4" /></a>
-                <a href='https://www.linkedin.com/in/abdul-mateen-khilji-8a743217b/'><i class='fa fa-linkedin-square icon' /></a>
+                <a href='https://github.com/JealousGx/react-phone-store'><i className='fa fa-github icon mt-1 git-icon' /></a>
+                <a href='https://twitter.com/mateen_khilji'><i class="fab fa-twitter icon mx-4 twitter-icon" /></a>
+                <a href='https://www.linkedin.com/in/abdul-mateen-khilji-8a743217b/'><i class='fa fa-linkedin-square icon in-icon' /></a>
                 </div>
             </div>
         </div>
